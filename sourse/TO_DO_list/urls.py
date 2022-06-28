df7155572fe72ba9e6bdfc08235b784e89c06363
abaibaiyshbekov <abay95poet@inbox.ru>
2022-06-28 18:35:14 +0600

    переписал код на views.py чтобы подробнее о задачах шла через PK, также дал название функциям

--- sourse/TO_DO_list/urls.py
+++ sourse/TO_DO_list/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from TO_DO_list.views import index_view, create_task, task_view
 
 urlpatterns = [
-    path("", index_view),
-    path('add/', create_task),
-    path('tasks/', task_view),
+    path("", index_view, name="index_view"),
+    path('add/', create_task, name="create_task"),
+    path('task/<int:pk>/', task_view, name="task_view"),
 ]
--- sourse/TO_DO_list/views.py
+++ sourse/TO_DO_list/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import render
 
@@ -13,17 +14,18 @@ def index_view(request):
     context = {"tasks": tasks}
     return render(request, "index.html", context)
 
-def task_view(request):
-    pass
+def task_view(request, pk):
+    task = Task.objects.get(pk=pk)
+    return render(request, "task.html", {"task": task})
 
 
 def create_task(request):
     if request.method == "GET":
         return render(request, "create.html", {"statuses": STATUS})
     else:
+        name_of_task = request.POST.get("name_of_task")
         description = request.POST.get("description")
         status =  request.POST.get("status")
         task_data = request.POST.get("task_data")
-        new_task = Task.objects.create(description=description, status=status, task_data=task_data)
-        context = {"task": new_task}
-        return render(request, "task.html", context)
+        new_task = Task.objects.create(name_of_task=name_of_task, description=description, status=status, task_data=task_data)
+        return redirect("task_view", pk=new_task.pk)
